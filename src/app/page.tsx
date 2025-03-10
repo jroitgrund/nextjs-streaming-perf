18cfa211a17ec1ab48e4ebdf7c14b09968482f2e
Jonathan Roitgrund <jroitgrund@gmail.com>
2025-03-10 17:22:35 +0100

    client component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Contents from "@/components/contents";
 import { setTimeout } from "timers/promises";
 
 export const dynamic = "force-dynamic";
@@ -7,5 +8,5 @@ export default async function Home() {
     await setTimeout(150);
     return "LOADED";
   })();
-  return <div className="text-8xl">{contents}</div>;
+  return <Contents contents={contents} />;
 }
---
+++ src/components/contents.tsx
@@ -0,0 +1,5 @@
+"use client";
+
+export default function Contents({ contents }: { contents: string }) {
+  return <div className="text-8xl">{contents}</div>;
+}
